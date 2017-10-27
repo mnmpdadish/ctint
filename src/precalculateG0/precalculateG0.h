0203c208bc2c5c43b8e74717e226605e7d7540dc
Maxime Charleboiis <mnmpdadish@hotmail.com>
2017-10-27 19:38:29 -0400

    Doing fourier transform without moment, don't work for now.

--- src/precalculateG0/precalculateG0.h
+++ src/precalculateG0/precalculateG0.h
@@ -11,7 +11,7 @@
 #include "../util/utilities.h"
 #include "../util/arrays/array.h"
 
-#define N_PTS 2000
+#define N_PTS 1000
 
 typedef struct {
   tMatrix tMat;
@@ -95,6 +95,7 @@ typedef struct {
   unsigned int n;
   //functionComplex * tau;
   functionComplex * functions;
+  double * M0; //zeroth moment
   double * M1; //first moment
   double * M2; //second moment
   double * M3; //third moment
@@ -105,6 +106,7 @@ void init_IndepFunctionComplex(IndepFunctionComplex * indepFun, Model * model) {
   indepFun->n         = model->greenSymMat.nIndep;
   //indepFun->tau       = (functionComplex *) malloc(indepFun->n * sizeof (functionComplex));
   indepFun->functions = (functionComplex *) malloc(indepFun->n * sizeof (functionComplex));
+  indepFun->M0 = (double *) malloc(indepFun->n * sizeof (double));
   indepFun->M1 = (double *) malloc(indepFun->n * sizeof (double));
   indepFun->M2 = (double *) malloc(indepFun->n * sizeof (double));
   indepFun->M3 = (double *) malloc(indepFun->n * sizeof (double));
@@ -115,6 +117,7 @@ void init_IndepFunctionComplex(IndepFunctionComplex * indepFun, Model * model) {
     int i=model->greenSymMat.iFirstIndep[k];
     int j=model->greenSymMat.jFirstIndep[k];
     nameGreenSymmetriesElement(&model->greenSymMat, i, j, indepFun->functions[k].name);
+    indepFun->M0[k] = 0.0;
     indepFun->M1[k] = 0.0;
     indepFun->M2[k] = 0.0;
     indepFun->M3[k] = 0.0;
@@ -125,30 +128,104 @@ void init_IndepFunctionComplex(IndepFunctionComplex * indepFun, Model * model) {
 void free_IndepFunctionComplex(IndepFunctionComplex * indepFun) {
   //free(indepFun->tau);
   free(indepFun->functions);
+  free(indepFun->M0);
   free(indepFun->M1);
   free(indepFun->M2);
   free(indepFun->M3);
 }
 
-void precalculate_G0(IndepFunctionComplex * g0, Model * model) {
+void calculateIndependant_G0_mat(IndepFunctionComplex * g0_mat, Model * model) {
   int k,n;
-  for(k=0; k<g0->n; k++){
+  for(k=0; k<g0_mat->n; k++){
     int i=model->greenSymMat.iFirstIndep[k];
     int j=model->greenSymMat.jFirstIndep[k];
     double tLoc_ij = calculate_tMatrixLoc_ij(&model->tMat, i, j);
     double a = -model->muAux + tLoc_ij;
-    g0->M1[k] = 1.0;
-    g0->M2[k] = a;
-    g0->M3[k] = a*a; //+hybFM;
+    g0_mat->M1[k] = 1.0;
+    g0_mat->M2[k] = a;
+    g0_mat->M3[k] = a*a; //+hybFM;
     for(n=0; n<N_PTS; n++){
-      double complex z = I*(2.*n+1)*M_PI/g0->beta;
+      double complex z = I*(2.*n+1)*M_PI/g0_mat->beta;
       double complex hyb = 0.0;
-      g0->functions[k].data[n] = 1./( z + model->muAux - tLoc_ij - hyb);
+      g0_mat->functions[k].data[n] = 1./( z + model->muAux - tLoc_ij - hyb);
       //if(n<100) printf("% 3.2e ",creal(g0->functions[k].data[n]));
     }
     printf("\n\n");
   }
 }
+
+
+void calculateInversFourierTransform(IndepFunctionComplex * g0_mat, IndepFunctionComplex * g0_tau) {
+  int k, n1, n2;
+  for(k=0; k<g0_mat->n; k++){
+    for(n1=0; n1<N_PTS; n1++){
+      g0_tau->functions[k].data[n1] = 0.0;
+      double tau = g0_mat->beta*n1/(N_PTS - 1);
+
+      for(n2=0; n2<N_PTS; n2++){
+        double omega_n2 = (2.*n2+1)*M_PI/g0_mat->beta;
+        double complex expFactor = (cos(-omega_n2*tau)+I*sin(-omega_n2*tau))/(g0_mat->beta) ;
+        g0_tau->functions[k].data[n1] += expFactor * g0_mat->functions[k].data[n2];
+      }
+    }
+  }
+}
+
+void removeMoments_G0_mat(IndepFunctionComplex * g0_mat) { // can be in mat(tsubara) or (imaginary time) tau, as long a moments are defined
+  int k, n;
+  for(n=0; n<N_PTS; n++){
+    double complex z = I*(2.*n+1)*M_PI/g0_mat->beta;
+    for(k=0; k<g0_mat->n; k++){  
+      g0_mat->functions[k].data[n] -= g0_mat->M0[k];
+      g0_mat->functions[k].data[n] -= g0_mat->M1[k]/z;
+      g0_mat->functions[k].data[n] -= g0_mat->M2[k]/(z*z);
+      g0_mat->functions[k].data[n] -= g0_mat->M3[k]/(z*z*z);
+    }
+  }
+}
+
+void addMoments_G0_tau(IndepFunctionComplex * g0_mat, IndepFunctionComplex * g0_tau) { // can be in mat(tsubara) or (imaginary time) tau, as long a moments are defined
+  int k, n;
+  double beta = g0_tau->beta;
+  for(n=0; n<N_PTS; n++){
+    double tau = g0_tau->beta*n/(N_PTS - 1);
+    for(k=0; k<g0_tau->n; k++){  
+      //g0_tau->functions[k].data[n] =0.0;
+      g0_tau->functions[k].data[n] += -g0_mat->M1[k]*0.5;
+      g0_tau->functions[k].data[n] +=  g0_mat->M2[k]*0.5*(tau-0.5*beta);
+      g0_tau->functions[k].data[n] +=  g0_mat->M3[k]*0.25*(beta-tau)*tau;
+    }
+  }
+}
+
+
+void calculateIndependant_G0_tau(IndepFunctionComplex * g0_mat, IndepFunctionComplex * g0_tau) {
+  g0_tau->beta = g0_mat->beta;
+  removeMoments_G0_mat(g0_mat);
+  calculateInversFourierTransform(g0_mat, g0_tau);
+  addMoments_G0_tau(g0_mat, g0_tau);
+}
+
+
+
+/*
+void IFT(const std::vector<_SiteVector>& bufferMAT, std::vector<_SiteVector>& bufferIT) {
+		int tauN = 0;
+		for(typename std::vector<_SiteVector>::iterator itIT = bufferIT.begin(); itIT != bufferIT.end(); itIT++, tauN++) {
+			double tau = beta_*static_cast<double>(tauN)/static_cast<double>(bufferIT.size() - 1);
+			itIT->clear();
+			
+			int frequencyMAT = 2*bufferMAT.size() - 1;
+			for(typename std::vector<_SiteVector>::const_reverse_iterator itMAT = bufferMAT.rbegin(); itMAT != bufferMAT.rend(); itMAT++, frequencyMAT -= 2) {
+				double omega = static_cast<double>(frequencyMAT)*M_PI/beta_;
+				
+				complex fact(2.*std::cos(-omega*tau)/beta_, 2.*std::sin(-omega*tau)/beta_);
+				for(_Site i = 0; i < _SiteVector::VEC_DIM; ++i) 
+					(*itIT)(i) += fact*(*itMAT)(i);
+			}
+		}
+	};
+	*/
 
 
 void writeToFile_IndepFunctionComplex(FILE *fileOut, IndepFunctionComplex * indepFun) {
@@ -161,12 +238,14 @@ void writeToFile_IndepFunctionComplex(FILE *fileOut, IndepFunctionComplex * inde
       
   for(n=0; n<N_PTS; n++){
     double omega_n = (2.*n+1)*M_PI/indepFun->beta;
-    double complex z = I*omega_n;
-    if(n<100) fprintf(fileOut, "% 3.6e  ", omega_n);
-    else break;
+    //double complex z = I*omega_n;
+    fprintf(fileOut, "% 3.6e  ", omega_n);
+    //else break;
     for(k=0; k<indepFun->n; k++){
-      if(n<100) fprintf(fileOut, "% 3.6e % 3.6e  ", creal(indepFun->functions[k].data[n]), cimag(indepFun->functions[k].data[n]));
-      if(n<100) fprintf(fileOut, "% 3.6e % 3.6e  ", creal(indepFun->M2[k]/(z*z)), cimag(indepFun->M1[k]/z+indepFun->M3[k]/(z*z*z)) );
+      fprintf(fileOut, "% 3.6e % 3.6e  ", creal(indepFun->functions[k].data[n]), cimag(indepFun->functions[k].data[n]));
+      //moments: (they fit)
+      //if(n<100) fprintf(fileOut, "% 3.6e % 3.6e  ", creal(indepFun->M2[k]/(z*z)), cimag(indepFun->M1[k]/z+indepFun->M3[k]/(z*z*z)) );
+      //if(n<100) fprintf(fileOut, "% 3.6e % 3.6e  ", creal(indepFun->functions[k].data[n]-indepFun->M2[k]/(z*z)), cimag(indepFun->functions[k].data[n]-(indepFun->M1[k]/z+indepFun->M3[k]/(z*z*z))) );
     }
     fprintf(fileOut,"\n");
   }
--- src/precalculateG0/precalculateG0_test.h
+++ src/precalculateG0/precalculateG0_test.h
@@ -17,14 +17,18 @@ int test_Plaquettes2x2(int verbose) {
   Model model;
   read_Model(file,&model);
   
-  IndepFunctionComplex g0;
-  init_IndepFunctionComplex(&g0, &model);
-  precalculate_G0(&g0, &model);
+  IndepFunctionComplex g0_mat, g0_tau;
+  init_IndepFunctionComplex(&g0_mat, &model);
+  init_IndepFunctionComplex(&g0_tau, &model);
+  calculateIndependant_G0_mat(&g0_mat, &model);
+  
   
   FILE *fileOut = fopen("green0.dat","w");
-  writeToFile_IndepFunctionComplex(fileOut, &g0);
+  calculateIndependant_G0_tau(&g0_mat, &g0_tau);
+  writeToFile_IndepFunctionComplex(fileOut, &g0_tau);
   
-  free_IndepFunctionComplex(&g0);
+  free_IndepFunctionComplex(&g0_mat);
+  free_IndepFunctionComplex(&g0_tau);
   free_Model(&model);
   
   return Nerror;
